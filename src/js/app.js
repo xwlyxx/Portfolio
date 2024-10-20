1fef021f650caa07760bc07bd730fed6785973ac
xwlyxx <pereira.wleite@gmail.com>
2024-10-20 14:45:17 -0300

    2.0

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,9 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            console.log(entry)
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.navbar ul');
+
+    // Toggle para abrir e fechar o menu
+    hamburger.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
     });
 });
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
